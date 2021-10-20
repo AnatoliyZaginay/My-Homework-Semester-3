1a11a2d12d463f6d2018dad364014bcf2cad2bd1
AnatoliyZaginay <anatoliy.zaginay@gmail.com>
2021-10-20 18:11:11 +0300

    Added MyTask

---
+++ Homeworks/Homework_3/Task_3/MyThreadPool.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Task_3
+{
+    public class MyThreadPool
+    {
+        private Thread[] threads;
+        private ConcurrentQueue<Action> taskQueue = new ConcurrentQueue<Action>();
+        private CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+        public MyThreadPool(int numberOfThreads)
+        {
+            if (numberOfThreads <= 0)
+            {
+                throw new ArgumentException("Incorrect number of threads");
+            }
+
+            threads = new Thread[numberOfThreads];
+
+            for (int i = 0; i < threads.Length; ++i)
+            {
+                //threads[i] = new Thread();
+                threads[i].Start();
+            }
+        }
+
+        private class MyTask<TResult> : IMyTask<TResult>
+        {
+            private Func<TResult> function;
+            private MyThreadPool threadPool;
+            private TResult result;
+            private ManualResetEvent resulatIsCalculated = new ManualResetEvent(false);
+            private Exception caughtExcpetion;
+
+            public MyTask(Func<TResult> function, MyThreadPool threadPool)
+            {
+                this.function = function;
+                this.threadPool = threadPool;
+            }
+
+            public void Run()
+            {
+                try
+                {
+                    result = function();
+                }
+                catch (Exception exception)
+                {
+                    caughtExcpetion = exception;
+                }
+
+                IsCompleted = true;
+                resulatIsCalculated.Set();
+                function = null;
+            }
+
+            public bool IsCompleted { get; private set; }
+
+            public TResult Result
+            {
+                get
+                {
+                    resulatIsCalculated.WaitOne();
+                    if (caughtExcpetion != null)
+                    {
+                        throw new AggregateException(caughtExcpetion);
+                    }
+                    return result;
+                }
+            }
+
+            public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> function)
+            {
+
+            }
+        }
+    }
+}
