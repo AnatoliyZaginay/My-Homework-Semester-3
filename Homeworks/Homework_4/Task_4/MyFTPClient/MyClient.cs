a4f1231e99f5a722b65b06d3d81e25f7a98e0908
AnatoliyZaginay <anatoliy.zaginay@gmail.com>
2021-10-26 23:29:39 +0300

    Added client (get work with deadlock)

---
+++ Homeworks/Homework_4/Task_4/MyFTPClient/MyClient.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace MyFTPClient
+{
+    public class MyClient
+    {
+        private TcpClient client;
+
+        public MyClient(string host, int port)
+        {
+            client = new(host, port);
+        }
+
+        public async Task<(int, List<(string, bool)>)> List(string directoryPath)
+        {
+            using var stream = client.GetStream();
+            using var reader = new StreamReader(stream);
+            using var writer = new StreamWriter(stream);
+            writer.AutoFlush = true;
+
+            await writer.WriteLineAsync($"1 {directoryPath}");
+            var response = await reader.ReadLineAsync();
+
+            if (response == "-1")
+            {
+                throw new ArgumentException("Incorrect directory");
+            }
+
+            var data = response.Split(' ');
+            var size = int.Parse(data[0]);
+
+            var list = new List<(string, bool)>();
+
+            for (int i = 1; i < data.Length; i += 2)
+            {
+                list.Add((data[i], bool.Parse(data[i + 1])));
+            }
+
+            return (size, list);
+        }
+
+        public async Task Get(string destination, string source)
+        {
+            using var stream = client.GetStream();
+            using var reader = new StreamReader(stream);
+            using var writer = new StreamWriter(stream);
+            writer.AutoFlush = true;
+
+            await writer.WriteLineAsync($"2 {source}");
+
+            var response = await reader.ReadLineAsync();
+
+            if (response == "-1")
+            {
+                throw new ArgumentException("File not exists");
+            }
+
+            using var fileStream = File.Create(destination);
+            await reader.BaseStream.CopyToAsync(fileStream);
+        }
+    }
+}
---
+++ Homeworks/Homework_4/Task_4/MyFTPClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace MyFTPClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Write("Enter the IP: ");
+            var ipString = Console.ReadLine();
+
+            if (!IPAddress.TryParse(ipString, out var ip))
+            {
+                Console.WriteLine("Incorrect IP");
+                return;
+            }
+
+            Console.Write("Enter the port: ");
+            var portString = Console.ReadLine();
+
+            if (!int.TryParse(portString, out var port) || port < 0 || port > 65535)
+            {
+                Console.WriteLine("Incorrect port");
+                return;
+            }
+
+            var client = new MyClient(ipString, port);
+        }
+    }
+}
