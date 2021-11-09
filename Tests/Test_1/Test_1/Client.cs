4b1ff4828c0475891ac955c348568eebc98673b2
AnatoliyZaginay <anatoliy.zaginay@gmail.com>
2021-11-09 17:30:11 +0300

    Added test_1

---
+++ Tests/Test_1/Test_1/Client.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Test_1
+{
+    /// <summary>
+    /// Client class.
+    /// </summary>
+    public class Client
+    {
+        private TcpClient client;
+
+        /// <summary>
+        /// Creaters a new client.
+        /// </summary>
+        public Client(string host, int port)
+        {
+            client = new TcpClient(host, port);
+        }
+
+        /// <summary>
+        /// Starts client.
+        /// </summary>
+        public void Run()
+        {
+            var stream = client.GetStream();
+
+            var send = Task.Run(async () => await Messaging.Send(stream, "Client"));
+            var receive = Task.Run(async () => await Messaging.Receive(stream, "Server"));
+            Task.WaitAny(send, receive);
+        }
+    }
+}
---
+++ Tests/Test_1/Test_1/Server.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Test_1
+{
+    /// <summary>
+    /// Server class.
+    /// </summary>
+    public class Server
+    {
+        private TcpListener listener;
+
+        /// <summary>
+        /// Creates a new server.
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="port"></param>
+        public Server(IPAddress ip, int port)
+        {
+            listener = new TcpListener(ip, port);
+        }
+
+        /// <summary>
+        /// Starts server.
+        /// </summary>
+        public async Task Run()
+        {
+            listener.Start();
+            var client = await listener.AcceptTcpClientAsync();
+            using var stream = client.GetStream();
+
+            var send = Task.Run(async () => await Messaging.Send(stream, "Server"));
+            var receive = Task.Run(async () => await Messaging.Receive(stream, "Client"));
+            Task.WaitAny(send, receive);
+
+            listener.Stop();
+        }
+    }
+}
